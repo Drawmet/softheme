42f638de78a169d270270e332a660bb30aae1fc1
Drawmet <vsezdorovo.kos@gmail.com>
2016-03-23 03:33:25 +0200

    Project to Softtheme2

---
+++ Softtheme2/Softtheme2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softtheme2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            number =  Int32.Parse(Console.ReadLine());
+            for (int k = number; k > 0; k--)
+            {
+                if (number % k == 0)
+                    Console.Write(k + " ");
+            }
+            Console.Read();
+        }
+    }
+}
